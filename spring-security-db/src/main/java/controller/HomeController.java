14c5cd1b8ac965cbd1f0f9f3a4516264fe380e0f
ManishaSingh14 <61121235+ManishaSingh14@users.noreply.github.com>
2020-03-31 17:16:01 +0530

    Add files via upload

---
+++ spring-security-db/src/main/java/controller/HomeController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+    @Controller
+    public class HomeController {
+        @RequestMapping(value = {"/", "/welcome**"}, method = RequestMethod.GET)
+        public ModelAndView defaultPage() {
+
+            ModelAndView model = new ModelAndView();
+            model.addObject("title", "Spring Security Login Form - Database Authentication");
+            model.addObject("message", "This is default page!");
+            model.setViewName("hello");
+            return model;
+        }
+        @RequestMapping(value = "/admin**", method = RequestMethod.GET)
+        public ModelAndView adminPage() {
+
+            ModelAndView model = new ModelAndView();
+            model.addObject("title", "Spring Security Login Form - Database Authentication");
+            model.addObject("message", "This page is for ROLE_ADMIN only!");
+            model.setViewName("admin");
+            return model;
+
+        }
+
+    }
---
+++ spring-security-db/src/main/java/controller/UserController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+    @RequestMapping(value = "/**", method = RequestMethod.GET)
+    public ModelAndView userPage() {
+
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Login Form - Database Authentication");
+        model.addObject("message", "This page is for ROLE_USER only!");
+        model.setViewName("hello");
+        return model;
+    }
+}
